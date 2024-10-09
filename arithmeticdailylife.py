b68b58d611cd428120f71d216664ffadb14047e9
HK-White <ghkwhite@gmail.com>
2024-10-09 15:14:58 -0700

    completed

---
+++ arithmeticdailylife.py
@@ -0,0 +1,16 @@
+# Task 1 
+peanut_butter = 2.18
+jelly = 3.49
+bread = 3.05
+
+total = peanut_butter + jelly + bread
+print("Your total is " + str(round(total, 2)))
+
+# Task 2
+balance = 10000
+interest_rate = 7.0
+
+interest_total = balance * (interest_rate / 100)
+
+total_end_of_year = balance + interest_total
+print("Your total at the end of the year is " + str(total_end_of_year))
